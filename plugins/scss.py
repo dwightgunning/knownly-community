29430935de361720e2123e9ecb78de0052708623
Dwight Gunning <dwight@dwightgunning.com>
2016-01-29 11:29:08 +1000

    Reorganised sass/css for improved modularity

--- plugins/scss.py
+++ plugins/scss.py
@@ -1,9 +1,5 @@
 import codecs
-import os
 import sys
-import pipes
-import shutil
-import subprocess
 
 from cactus.utils.filesystem import fileList
 
@@ -17,22 +13,23 @@ sudo easy_install pyScss
 """
 
 try:
-	from scss import Scss
+    from scss import Scss
 except:
-	sys.exit("Could not find pyScss, please install: sudo easy_install pyScss")
+    sys.exit("Could not find pyScss, please install: sudo easy_install pyScss")
 
 
 CSS_PATH = './static/css'
 
 for path in fileList(CSS_PATH):
-	
-	if not path.endswith('.scss'):
-		continue
-	
-	with codecs.open(path, 'r', 'utf-8') as f:
-		data = f.read()
-	
-	css = Scss().compile(data)
 
-	with codecs.open(path.replace('.scss', '.css'), 'w', 'utf-8') as f:
-		f.write(css)
+    if path.find('_') >= 0 or not path.endswith('.scss'):
+        continue
+
+    with codecs.open(path, 'r', 'utf-8') as f:
+        data = f.read()
+
+    css = Scss().compile(data)
+
+    with codecs.open(path.replace('.scss', '.css'), 'w', 'utf-8') as f:
+        f.write(css)
+        f.write('\n')
