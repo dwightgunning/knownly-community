069193a01732ac713685fd6903e1973aaecabfa4
Dwight Gunning <dwight@dwightgunning.com>
2016-02-14 23:12:34 +1000

    Added disqus comment system to posts and guides

--- plugins/page_context.py
+++ plugins/page_context.py
@@ -1,18 +1,18 @@
-#coding:utf-8
-
 def preBuildPage(page, context, data):
     """
-    Updates the context of the page to include: the page itself as {{ CURRENT_PAGE }}
+    Updates the context of the page to include: the page itself
+    as {{ CURRENT_PAGE }}
     """
 
     # This will run for each page that Cactus renders.
-    # Any changes you make to context will be passed to the template renderer for this page.
+    # Any changes you make to context will be passed to the template
+    # renderer for this page.
 
     extra = {
         "CURRENT_PAGE": page,
         "MIXPANEL_TOKEN": page.site.config.get('mixpanel-token', ''),
-        "GA_TRACKING_ID": page.site.config.get('ga-tracking-id', '')
-        # Add your own dynamic context elements here!
+        "GA_TRACKING_ID": page.site.config.get('ga-tracking-id', ''),
+        "DISQUS_SHORTNAME": page.site.config.get('disqus-shortname', '')
     }
 
     context.update(extra)
